21585c1b4a587061f4561ac000f2e4bec91829df
makroterno <kagan.akar1@gmail.com>
2024-05-01 13:54:57 +0300

    Initial commit

---
+++ scripts/enemy.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+const SPEED = 60
+
+var direction = 1
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	position.x += direction * SPEED * delta
