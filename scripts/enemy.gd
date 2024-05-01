0c5c6a35c8e077ebbab718ad285f12b4e1121342
makroterno <kagan.akar1@gmail.com>
2024-05-01 14:59:42 +0300

    Finish game

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -1,6 +1,9 @@
 extends Node2D
 
 const SPEED = 60
+@onready var ray_cast_right = $RayCastRight
+@onready var ray_cast_left = $RayCastLeft
+@onready var animated_sprite = $AnimatedSprite2D
 
 var direction = 1
 # Called when the node enters the scene tree for the first time.
@@ -10,4 +13,10 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	if ray_cast_right.is_colliding():
+		direction = -1
+		animated_sprite.flip_h = true
+	if ray_cast_left.is_colliding():
+		direction = 1
+		animated_sprite.flip_h = false
 	position.x += direction * SPEED * delta
